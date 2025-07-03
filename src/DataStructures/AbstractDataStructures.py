500f442c20b9c16994b014729c0413ce21ad358f
Francisco Proboste <fjproboste@gmail.com>
2025-07-02 23:27:09 -0300

    Priority Queue to be used in a-star

---
+++ src/DataStructures/AbstractDataStructures.py
@@ -0,0 +1,54 @@
+import heapq
+
+class PriorityQueue:
+    """
+      Implements a priority queue data structure. Each inserted item
+      has a priority associated with it and the client is usually interested
+      in quick retrieval of the lowest-priority item in the queue. This
+      data structure allows O(1) access to the lowest-priority item.
+    """
+    def __init__(self, reverse=False):
+        self.heap = []
+        self.count = 0
+        self.reverse = reverse
+
+    def push(self, item, priority):
+        # If reverse=True, we want higher priority items to come out first
+        if self.reverse:
+            priority = -priority
+        entry = (priority, self.count, item)
+        heapq.heappush(self.heap, entry)
+        self.count += 1
+
+    def pop(self):
+        (_, _, item) = heapq.heappop(self.heap)
+        return item
+
+    def isEmpty(self):
+        return len(self.heap) == 0
+
+    def update(self, item, priority):
+        # If item already in priority queue with higher priority, update its priority and rebuild the heap.
+        # If item already in priority queue with equal or lower priority, do nothing.
+        # If item not in priority queue, do the same thing as self.push.
+        for index, (p, c, i) in enumerate(self.heap):
+            if i == item:
+                if p <= priority:
+                    break
+                del self.heap[index]
+                self.heap.append((priority, c, item))
+                heapq.heapify(self.heap)
+                break
+        else:
+            self.push(item, priority)
+
+    # Alias methods to match the A* search code
+    def enqueue(self, item, priority):
+        self.push(item, priority)
+
+    def dequeue(self):
+        return self.pop()
+
+    @property
+    def size(self):
+        return len(self.heap) 
